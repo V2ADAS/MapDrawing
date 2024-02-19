18e6f18d3188289311900fe23f077cf8e6c395c6
Khaled Basiony <basiony.khaled@outlook.com>
2024-02-19 15:58:44 +0200

    make plotting window update the plot instead of replotting

--- draw.py
+++ draw.py
@@ -21,9 +21,25 @@ class MapDrawer:
     data_mem = []
     ax = None
     fig = None
+    scatter = None
 
     def __init__(self):
         matplotlib.use("QtAgg")
+        # plt.ion()
+        # fig, ax = plt.subplots()
+        # x, y = [], []
+        # sc = ax.scatter(x, y)
+        # plt.xlim(0, 10)
+        # plt.ylim(0, 10)
+        # plt.draw()
+        # for _ in range(100):
+        #     x.append(np.random.rand(1)*10)
+        #     y.append(np.random.rand(1)*10)
+        #     sc.set_offsets(np.c_[x, y])
+        #     fig.canvas.draw_idle()
+        #     plt.pause(0.1)
+        # plt.waitforbuttonpress()
+
         self.fig, self.ax = plt.subplots()
 
         # Set the dimensions of the car chassis (in cm)
@@ -44,37 +60,41 @@ class MapDrawer:
         """
         Runs the main drawing cycle
         """
-        with open(sys.argv[1], "r") as file:
+        with open(sys.argv[1], "r", encoding='utf8') as file:
+            # Read data from the file
+            data = self.get_input(file)
+            self.data_mem = np.append(self.data_mem, data, 0)[-MAX_HISTORY:]
+
+            # Draw the car chassis with a specific face color
+            self.draw_car_chassis(facecolor="lightblue")
+
+            # Plot the wheel module at different positions
+            for position in self.positions:
+                self.plot_wheel(position[0], position[1])
+
+            # Plot data points
+            self.update_data()
+
+            # Set the aspect ratio to be equal, so the wheel doesn't get distorted
+            self.ax.set_aspect("equal", adjustable="box")
+
+            # Set the axis limits based on the ultrasonic range
+            self.ax.set_xlim(-250, 250)
+            self.ax.set_ylim(-250, 250)
+
+            # Show the plot
+            plt.show(block=False)
+            plt.pause(1 / 60)
+
             while True:
                 # Read data from the file
                 data = self.get_input(file)
                 self.data_mem = np.append(self.data_mem, data, 0)[-MAX_HISTORY:]
 
-                # Get current axis
-                ax = plt.gca()
-
-                # Clear previous plot
-                ax.clear()
-
-                # Draw the car chassis with a specific face color
-                self.draw_car_chassis(facecolor="lightblue")
-
-                # Plot the wheel module at different positions
-                for position in self.positions:
-                    self.plot_wheel(position[0], position[1])
-
                 # Plot data points from memory after recent input
-                self.update_data(ax)
+                self.update_data()
 
-                # Set the aspect ratio to be equal, so the wheel doesn't get distorted
-                ax.set_aspect("equal", adjustable="box")
-
-                # Set the axis limits based on the ultrasonic range
-                ax.set_xlim(-450, 450)
-                ax.set_ylim(-450, 450)
-
-                # Show the plot
-                plt.show(block=False)
+                self.fig.canvas.draw_idle()
                 plt.pause(1 / 60)
 
     def draw_car_chassis(self, facecolor="blue"):
@@ -117,7 +137,7 @@ class MapDrawer:
         wheel = patches.Polygon(vertices, edgecolor="none", facecolor="blue")
         self.ax.add_patch(wheel)
 
-    def update_data(self, ax):
+    def update_data(self):
         """
         Update axes with current data
         """
@@ -126,9 +146,16 @@ class MapDrawer:
             np.linspace(0.01, 1, self.data_mem.shape[0]) ** 2,
             data_mem_plt.shape[0] / self.data_mem.shape[0],
         )
-        ax.scatter(
-            data_mem_plt[:, 0], data_mem_plt[:, 1], color="red", alpha=alphas, s=4
-        )
+
+        if self.scatter is None:
+            self.scatter = self.ax.scatter(
+                data_mem_plt[:, 0], data_mem_plt[:, 1],
+                color="red",
+                # alpha=alphas,
+                s=4,
+            )
+        else:
+            self.scatter.set(offsets=data_mem_plt, alpha=alphas)
 
     def get_input(self, file):
         """
